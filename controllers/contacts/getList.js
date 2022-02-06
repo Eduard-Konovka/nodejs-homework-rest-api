bfd73bf6bcb2193ed0734cee871fd94d431bb353
Eduard-Konovka <ed098ua@gmail.com>
2022-02-06 13:54:50 +0200

    Updated getList, adds sort query parameters

--- controllers/contacts/getList.js
+++ controllers/contacts/getList.js
@@ -2,15 +2,24 @@ const { Contact } = require("../../models");
 
 const getList = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 20, favorite } = req.query;
+  const { page = 1, limit = 20, favorite, sort } = req.query;
   const skip = (page - 1) * limit;
 
   const query = favorite ? { owner: _id, favorite } : { owner: _id };
 
-  const result = await Contact.find(query, "-createdAt -updatedAt", {
-    skip,
-    limit: Number(limit),
-  }).populate("owner", "_id email subscription");
+  const result = await Contact.find(query)
+    .select("-createdAt -updatedAt")
+    .skip(skip)
+    .limit(parseInt(limit))
+    .sort(sort)
+    .populate("owner", "_id email subscription");
+  // // Альтернатива:
+  // const result = await Contact.find(query, "-createdAt -updatedAt", {
+  //   skip,
+  //   limit: Number(limit),
+  // })
+  //   .sort({ name: 1 })
+  //   .populate("owner", "_id email subscription");
 
   res.json({
     status: "success",
